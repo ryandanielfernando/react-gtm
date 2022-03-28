a448393ff38ddc193e1a26ab877678fff5e14f04
ryandanielfernando <ryandanielfernando@gmail.com>
2022-03-28 11:36:08 -0700

    add event listener

--- src/Snippets.js
+++ src/Snippets.js
@@ -18,6 +18,10 @@ const Snippets = {
         w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js', ${JSON.stringify(events).slice(1, -1)}});
         var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
         j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl+'${gtm_auth}${gtm_preview}&gtm_cookies_win=x';
+        j.addEventListener('load', function() {
+          var _ge = new CustomEvent('gtm_loaded', { bubbles: true });
+          d.dispatchEvent(_ge);
+        });
         f.parentNode.insertBefore(j,f);
       })(window,document,'script','${dataLayerName}','${id}');`
   
